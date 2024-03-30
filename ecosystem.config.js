10d6376390b5c295d7df124cf74f42cef0c5dabb
JQiue <861947542@qq.com>
2024-03-30 15:47:26 +0800

    chore: pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,23 @@
+require("dotenv").config();
+
+const env = {
+  MYSQL_HOST: process.env.MYSQL_HOST,
+  MYSQL_DB: process.env.MYSQL_HOST,
+  MYSQL_USER: process.env.MYSQL_HOST,
+  MYSQL_PASSWORD: process.env.MYSQL_HOST,
+  SMTP_SERVICE: process.env.MYSQL_HOST,
+  SMTP_USER: process.env.MYSQL_HOST,
+  SMTP_PASS: process.env.MYSQL_HOST,
+  SITE_NAME: process.env.MYSQL_HOST,
+  SITE_URL: process.env.MYSQL_HOST,
+  AUTHOR_EMAIL: process.env.MYSQL_HOST,
+  SENDER_EMAIL: process.env.MYSQL_HOST,
+  SENDER_NAME: process.env.MYSQL_HOST,
+  LEVELS: process.env.MYSQL_HOST,
+  DISABLE_REGION: process.env.MYSQL_HOST,
+  IPQPS: process.env.MYSQL_HOST,
+};
+
 module.exports = {
   apps: [
     {
@@ -6,6 +26,7 @@ module.exports = {
       max_memory_restart: "100M",
       error_file: "./pm2.error.log",
       out_file: "./pm2.out.log",
+      env,
     },
   ],
 };
